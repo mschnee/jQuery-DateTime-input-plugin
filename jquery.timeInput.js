061bbd3866c694177a0dd253d157c2d2c106b93d
Matthew Schnee <matthew.schnee@gmail.com>
2012-02-26 10:54:45 -0600

    More work on initial values and events.
    This commit adds strtodate which is a modified strtotime from phpjs.
    
    Cleans up initial values, but I'm sure it's not done yet.
    
    Sets up events.  I need to play with local/private functions for a bit to figure out the best way to do this.

--- jquery.timeInput.js
+++ jquery.timeInput.js
@@ -557,6 +557,193 @@ Oy
 }
 
 /**
+ * strtodate was originally strtotime from phpjs.org
+ * I modified it to accept a Date() as a parameter for `now` and to return a 
+ * Date() instead of a timestamp.
+ */
+function strtodate (str, now) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Caio Ariede (http://caioariede.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: David
+    // +   improved by: Caio Ariede (http://caioariede.com)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Wagner B. Soares
+    // +   bugfixed by: Artur Tchernychev
+    // ++  improved by: Matthew Schnee <matthew.schnee@gmail.com> to accept a Date object as the `now` parameter. 
+    // %        note 1: Examples all have a fixed timestamp to prevent tests to fail because of variable time(zones)
+    // *     example 1: strtotime('+1 day', 1129633200);
+    // *     returns 1: 1129719600
+    // *     example 2: strtotime('+1 week 2 days 4 hours 2 seconds', 1129633200);
+    // *     returns 2: 1130425202
+    // *     example 3: strtotime('last month', 1129633200);
+    // *     returns 3: 1127041200
+    // *     example 4: strtotime('2009-05-04 08:30:00');
+    // *     returns 4: 1241418600
+    var i, l, match, s, parse = '';
+
+    str = str.replace(/\s{2,}|^\s|\s$/g, ' '); // unecessary spaces
+    str = str.replace(/[\t\r\n]/g, ''); // unecessary chars
+    if (str === 'now') {
+        return now === null || isNaN(now) ? new Date().getTime() / 1000 | 0 : now | 0;
+    } else if (!isNaN(parse = Date.parse(str))) {
+        return new Date(str);
+    } else if (now) {
+        now = (typeof(timestamp) == 'object') ? new Date(now):new Date(now * 1000); // Accept PHP-style seconds
+    } else {
+        now = new Date();
+    }
+
+    str = str.toLowerCase();
+
+    var __is = {
+        day: {
+            'sun': 0,
+            'mon': 1,
+            'tue': 2,
+            'wed': 3,
+            'thu': 4,
+            'fri': 5,
+            'sat': 6
+        },
+        mon: [
+            'jan',
+            'feb',
+            'mar',
+            'apr',
+            'may',
+            'jun',
+            'jul',
+            'aug',
+            'sep',
+            'oct',
+            'nov',
+            'dec'
+        ]
+    };
+
+    var process = function (m) {
+        var ago = (m[2] && m[2] === 'ago');
+        var num = (num = m[0] === 'last' ? -1 : 1) * (ago ? -1 : 1);
+        
+        switch (m[0]) {
+        case 'last':
+        case 'next':
+            switch (m[1].substring(0, 3)) {
+            case 'yea':
+                now.setFullYear(now.getFullYear() + num);
+                break;
+            case 'wee':
+                now.setDate(now.getDate() + (num * 7));
+                break;
+            case 'day':
+                now.setDate(now.getDate() + num);
+                break;
+            case 'hou':
+                now.setHours(now.getHours() + num);
+                break;
+            case 'min':
+                now.setMinutes(now.getMinutes() + num);
+                break;
+            case 'sec':
+                now.setSeconds(now.getSeconds() + num);
+                break;
+            case 'mon':
+                if (m[1] === "month") {
+                    now.setMonth(now.getMonth() + num);
+                    break;
+                }
+                // fall through
+            default:
+                var day = __is.day[m[1].substring(0, 3)];
+                if (typeof day !== 'undefined') {
+                    var diff = day - now.getDay();
+                    if (diff === 0) {
+                        diff = 7 * num;
+                    } else if (diff > 0) {
+                        if (m[0] === 'last') {
+                            diff -= 7;
+                        }
+                    } else {
+                        if (m[0] === 'next') {
+                            diff += 7;
+                        }
+                    }
+                    now.setDate(now.getDate() + diff);
+                    now.setHours(0, 0, 0, 0); // when jumping to a specific last/previous day of week, PHP sets the time to 00:00:00
+                }
+            }
+            break;
+
+        default:
+            if (/\d+/.test(m[0])) {
+                num *= parseInt(m[0], 10);
+
+                switch (m[1].substring(0, 3)) {
+                case 'yea':
+                    now.setFullYear(now.getFullYear() + num);
+                    break;
+                case 'mon':
+                    now.setMonth(now.getMonth() + num);
+                    break;
+                case 'wee':
+                    now.setDate(now.getDate() + (num * 7));
+                    break;
+                case 'day':
+                    now.setDate(now.getDate() + num);
+                    break;
+                case 'hou':
+                    now.setHours(now.getHours() + num);
+                    break;
+                case 'min':
+                    now.setMinutes(now.getMinutes() + num);
+                    break;
+                case 'sec':
+                    now.setSeconds(now.getSeconds() + num);
+                    break;
+                }
+            } else {
+                return false;
+            }
+            break;
+        }
+        return true;
+    };
+
+    match = str.match(/^(\d{2,4}-\d{2}-\d{2})(?:\s(\d{1,2}:\d{2}(:\d{2})?)?(?:\.(\d+))?)?$/);
+    if (match !== null) {
+        if (!match[2]) {
+            match[2] = '00:00:00';
+        } else if (!match[3]) {
+            match[2] += ':00';
+        }
+
+        s = match[1].split(/-/g);
+
+        s[1] = __is.mon[s[1] - 1] || s[1];
+        s[0] = +s[0];
+
+        s[0] = (s[0] >= 0 && s[0] <= 69) ? '20' + (s[0] < 10 ? '0' + s[0] : s[0] + '') : (s[0] >= 70 && s[0] <= 99) ? '19' + s[0] : s[0] + '';
+        return parseInt(this.strtotime(s[2] + ' ' + s[1] + ' ' + s[0] + ' ' + match[2]) + (match[4] ? match[4] / 1000 : ''), 10);
+    }
+
+    var regex = '([+-]?\\d+\\s' + '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?' + '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday' + '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday)' + '|(last|next)\\s' + '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?' + '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday' + '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday))' + '(\\sago)?';
+
+    match = str.match(new RegExp(regex, 'gi')); // Brett: seems should be case insensitive per docs, so added 'i'
+    if (match === null) {
+        return false;
+    }
+
+    for (i = 0, l = match.length; i < l; i++) {
+        if (!process(match[i].split(' '))) {
+            return false;
+        }
+    }
+
+    return now;
+}
+
+/**
  * jQuery.timeInput.js v0.0.1
  * 
  * Copyright (C) 2012 Matthew Schnee <matthew.schnee@gmail.com>
@@ -610,7 +797,7 @@ Oy
          */
         init: function(params) {
             var settings = $.extend({
-                format: "%I:%M %p",         /* the format according to strftime() */
+                format: "%m/%d/%y %I:%M %p",         /* the format according to strftime() */
                 defaultIsMidnight: false,   /* use the current time (true) or midnight(false) as a fallback */
             },params);
             
@@ -627,12 +814,24 @@ Oy
                         element: Self,
                         displayFormat: settings.format,
                         tokens: [],
-                        ds: new Date( Self.val() )
+                        ds: null
                     });
+                    
                     data = Self.data('timeInput');
+                    
+                    // construct the Date object
+                    data.ds = new Date(Self.val());
                     if(data.ds=="Invalid Date") {
+                        data.ds = new Date(parseInt(Self.val()));
+                    }
+                    if(data.ds=="Invalid Date") {
+                        data.ds = strtodate(Self.val(),new Date());
+                    }
+                    if(!data.ds || data.ds=="Invalid Date") {
                         data.ds = settings.defaultIsMidnight?new Date(_current.getFullYear(),_current.getMonth(),_current.getDate(),0,0,0,0):new Date()
                     }
+                    
+                    data.originalDate = data.ds;
                     
                     // tokenization.  First do string replacement according to phpjs.
                     while (settings.format.match(/%[cDFhnrRtTxX]/)) {
@@ -674,7 +873,10 @@ Oy
                 } /* </ if( ! data ) >
                 
                 /* bind the elements to keypress */
-                $(window).bind('keypress.timeInput', methods.keypress);
+                Self.bind('keydown.timeInput', methods.keydown);
+                Self.bind('focus.timeInput', methods.focus);
+                Self.bind('blur.timeInput', methods.blur);
+                Self.bind('click.timeInput', methods.mouseclick);
                 
                 /* Update the input element */
                 Self.timeInput("updateDisplay");
@@ -698,13 +900,25 @@ Oy
         /**
          * Handle the element's keypress.
          */
-        keypress: function(event,callback_function) {
+        keydown: function(event) {
             console.log(event);
-            /* do things */
-            if(callback_function)
-                callback_function.apply(this,event);
+            var key = {left: 37, up: 38, right: 39, down: 40, tab:9, enter:13, backspace: 8 };
+            switch(event.which) {
+                case(key.left): retreat(); break;
+                case(key.right): advance(); break;
+                case(key.up): increment(); break;
+                case(key.down): decrement(); break;
+            }
         },
- 
+        focus: function(event) {
+            console.log(event);
+        },
+        blur: function(event) {
+            console.log(event);
+        },
+        click: function(event) {
+            console.log(event);
+        },
         /**
          * Destroy and unbind the events
          */
@@ -713,7 +927,7 @@ Oy
                 var Self = $(this),
                     data = Self.data('timeInput');
                     
-                $(window).unbind('timeInput');
+                Self.unbind('.timeInput');
                 Self.removeData('timeInput');
             });
         }
